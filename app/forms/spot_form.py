2ea985d2b6ccaebc801b6479815dbc9b29091ab1
Yasamine <yasamine.97.cruz@gmail.com>
2022-12-04 16:02:49 -0800

    price updated

--- app/forms/spot_form.py
+++ app/forms/spot_form.py
@@ -1,15 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField
+from wtforms import StringField, FloatField
 from wtforms.validators import DataRequired
 
 class SpotForm(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
     description = StringField('description', validators=[DataRequired()])
-    price = IntegerField('price', validators=[DataRequired()])
+    price = FloatField('price', validators=[DataRequired()])
     city = StringField('city', validators=[DataRequired()])
     state = StringField('state', validators=[DataRequired()])
     country = StringField('country', validators=[DataRequired()])
     amenities = StringField('amenities', validators=[DataRequired()])
     type = StringField('type', validators=[DataRequired()])
     activities = StringField('activities', validators=[DataRequired()])
-    url = StringField('url', validators=[DataRequired()])
